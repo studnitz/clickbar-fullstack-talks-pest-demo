9fbe04e391c9fd0dedd43c8f45f5eda8be149648
Alexander von Studnitz <studnitz@hey.com>
2022-01-28 16:24:36 +0100

    Add birthDay and age() to Person

--- app/Person.php
+++ app/Person.php
@@ -2,11 +2,14 @@
 
 namespace App;
 
+use Carbon\CarbonImmutable;
+
 class Person
 {
     public function __construct(
         public string $firstName,
         public string $lastName,
+        public ?CarbonImmutable $birthDay,
     )
     { }
 
@@ -18,5 +21,10 @@ class Person
     public function greet(): string
     {
         return "Hello, {$this->fullName()}!";
+    }
+
+    public function age(): int
+    {
+        return $this->birthDay->diffInYears(today());
     }
 }
--- tests/Unit/PersonTest.php
+++ tests/Unit/PersonTest.php
@@ -1,13 +1,21 @@
 <?php
 
 use App\Person;
+use Carbon\CarbonImmutable;
 
 test('max is greeted', function () {
-    $person = new Person('Max', 'Mustermann');
+    $person = new Person('Max', 'Mustermann', CarbonImmutable::parse('1990-01-01'));
     $this->assertSame('Hello, Max Mustermann!', $person->greet());
 });
 
 test('maria is greeted', function () {
-    $person = new Person('Maria', 'Musterfrau');
+    $person = new Person('Maria', 'Musterfrau', CarbonImmutable::parse('1990-01-01'));
     $this->assertSame('Hello, Maria Musterfrau!', $person->greet());
 });
+
+it('can get the right age for Max', function() {
+    $person = new Person('Max', 'Mustermann', CarbonImmutable::parse('1990-01-01'));
+    $this->assertEquals(32, $person->age());
+});
+
+it('greets a person with Happy Birthday on their birthday');
