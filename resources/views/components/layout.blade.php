5acba6ae39dae15a3960406b9953e6ddd96a10ac
Alexander von Studnitz <studnitz@hey.com>
2022-02-01 17:48:40 +0100

    Add tailwind

---
+++ resources/views/components/layout.blade.php
@@ -0,0 +1,19 @@
+<!DOCTYPE html>
+<html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
+    <head>
+        <meta charset="utf-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1">
+
+
+        <link href="{{ asset('css/app.css') }}" rel="stylesheet">
+        <title>Pest Talk</title>
+    </head>
+    <body class="antialiased">
+        <div class="relative flex items-top justify-center min-h-screen bg-gray-100 dark:bg-gray-900 sm:items-center py-4 sm:pt-0">
+
+            <div class="max-w-6xl mx-auto sm:px-6 lg:px-8">
+                {{ $slot }}
+            </div>
+        </div>
+    </body>
+</html>
